fbb2bf030f141cb71e53e47cae68ab1658f4391d
Aniruddha Humane <ani.humane@outlook.com>
2018-04-28 17:50:56 +0530

    Navigation Added

--- JS/main.js
+++ JS/main.js
@@ -1,28 +1,30 @@
-$('#menu-toggle').click(function(){
+$('#menu-toggle').click(function () {
   $(this).toggleClass('open');
   $(".menu").toggleClass('addmenuani');
-  $("html,body").toggleClass("overhidden");
-  //$("section.makeitwhite").addClass("makeitwhiteon");
 });
 
+$("a.mamama").click(function () {
+  $('#menu-toggle').removeClass('open');
+  $(".menu").removeClass('addmenuani');
+})
+
 $("section.menu nav.menunav ul li").hover(
-  function(){
-    $($(this).children()[1]).css("transform","scaleX(15)");
+  function () {
+    $($(this).children()[1]).css("transform", "scaleX(15)");
   },
-  function(){
-    $($(this).children()[1]).css("transform","scaleX(1)");
+  function () {
+    $($(this).children()[1]).css("transform", "scaleX(1)");
   }
 )
 
-$(".maindown").on("click",function(){
-})
+$(".maindown").on("click", function () {})
 
 
 /*SKILLS*/
 
-var skills = [
-  {"header" : "INTERESTS",
-    "captions" : [
+var skills = [{
+    "header": "INTERESTS",
+    "captions": [
       "Compilers",
       "Web",
       "CN",
@@ -30,7 +32,7 @@ var skills = [
       "AI",
       "Competitve"
     ],
-    "values" : [
+    "values": [
       0.80,
       0.80,
       0.80,
@@ -39,8 +41,9 @@ var skills = [
       0.85
     ]
   },
-  {"header" : "LANGUAGES",
-    "captions" : [
+  {
+    "header": "LANGUAGES",
+    "captions": [
       "C",
       "C++",
       "Python",
@@ -48,7 +51,7 @@ var skills = [
       "MEAN",
       "MySQL"
     ],
-    "values" : [
+    "values": [
       0.80,
       0.85,
       0.70,
@@ -57,8 +60,9 @@ var skills = [
       0.85
     ]
   },
-  {"header" : "MISC",
-    "captions" : [
+  {
+    "header": "MISC",
+    "captions": [
       "Sketching",
       "Anime",
       "Gaming",
@@ -66,7 +70,7 @@ var skills = [
       "Singing",
       "Reading"
     ],
-    "values" : [
+    "values": [
       0.95,
       0.90,
       0.85,
@@ -81,13 +85,15 @@ var pentagonIndex = 0;
 var valueIndex = 0;
 var width = 0;
 var height = 0;
-var radOffset = Math.PI/2
+var radOffset = Math.PI / 2
 var sides = 6; // Number of sides in the polygon
-var theta = 2 * Math.PI/sides; // radians per section
+var theta = 2 * Math.PI / sides; // radians per section
 
 function getXY(i, radius) {
-  return {"x": Math.cos(radOffset +theta * i) * radius*width + width/2,
-    "y": Math.sin(radOffset +theta * i) * radius*height + height/2};
+  return {
+    "x": Math.cos(radOffset + theta * i) * radius * width + width / 2,
+    "y": Math.sin(radOffset + theta * i) * radius * height + height / 2
+  };
 }
 
 var hue = [];
@@ -95,29 +101,29 @@ var hueOffset = 25;
 
 for (var s in skills) {
   $(".content").append('<div class="pentagon animated zoomIn" id="interests"><div class="header"></div><canvas class="pentCanvas"/></div>');
-  hue[s] = (hueOffset + s * 255/skills.length) % 255;
+  hue[s] = (hueOffset + s * 255 / skills.length) % 255;
 }
 
-$(".pentagon").each(function(index){
+$(".pentagon").each(function (index) {
   width = $(this).width();
   height = $(this).height();
   var ctx = $(this).find('canvas')[0].getContext('2d');
   ctx.canvas.width = width;
   ctx.canvas.height = height;
-  ctx.font="bolder 20px 'Raleway'";
-  if($(window).width()<1400){
-    ctx.font="bolder 15px 'Raleway'";
+  ctx.font = "bolder 20px 'Raleway'";
+  if ($(window).width() < 1400) {
+    ctx.font = "bolder 15px 'Raleway'";
   }
-  ctx.textAlign="center";
+  ctx.textAlign = "center";
 
   /*** LABEL ***/
-  color = "hsl("+hue[pentagonIndex]+", 100%, 50%)";
+  color = "hsl(" + hue[pentagonIndex] + ", 100%, 50%)";
   ctx.fillStyle = color;
-  ctx.fillText(skills[pentagonIndex].header, width/2, 15);
+  ctx.fillText(skills[pentagonIndex].header, width / 2, 15);
 
-  ctx.font="bolder 15px Raleway";
-  if($(window).width()<1400){
-    ctx.font="bolder 12px 'Raleway'";
+  ctx.font = "bolder 15px Raleway";
+  if ($(window).width() < 1400) {
+    ctx.font = "bolder 12px 'Raleway'";
   }
 
   /*** PENTAGON BACKGROUND ***/
@@ -125,16 +131,16 @@ $(".pentagon").each(function(index){
     // For each side, draw two segments: the side, and the radius
     ctx.beginPath();
     xy = getXY(i, 0.3);
-    colorJitter = 25 + theta*i*2;
-    color = "hsl("+hue[pentagonIndex]+",100%," + colorJitter + "%)";
+    colorJitter = 25 + theta * i * 2;
+    color = "hsl(" + hue[pentagonIndex] + ",100%," + colorJitter + "%)";
     ctx.fillStyle = color;
     ctx.strokeStyle = color;
-    ctx.moveTo(0.5*width, 0.5*height); //center
+    ctx.moveTo(0.5 * width, 0.5 * height); //center
     ctx.lineTo(xy.x, xy.y);
-    xy = getXY(i+1, 0.3);
+    xy = getXY(i + 1, 0.3);
     ctx.lineTo(xy.x, xy.y);
     xy = getXY(i, 0.37);
-    ctx.fillText(skills[ pentagonIndex].captions[valueIndex],xy.x, xy.y +5);
+    ctx.fillText(skills[pentagonIndex].captions[valueIndex], xy.x, xy.y + 5);
     valueIndex++;
     ctx.closePath();
     ctx.fill();
@@ -148,11 +154,11 @@ $(".pentagon").each(function(index){
   ctx.lineWidth = 5;
   var value = skills[pentagonIndex].values[valueIndex];
   xy = getXY(i, value * 0.3);
-  ctx.moveTo(xy.x,xy.y);
+  ctx.moveTo(xy.x, xy.y);
   /*** SKILL GRAPH ***/
   for (var i = 0; i < sides; i++) {
     xy = getXY(i, value * 0.3);
-    ctx.lineTo(xy.x,xy.y);
+    ctx.lineTo(xy.x, xy.y);
     valueIndex++;
     value = skills[pentagonIndex].values[valueIndex];
   }
@@ -164,16 +170,16 @@ $(".pentagon").each(function(index){
 });
 
 /* MODAL */
-var Modal = (function() {
+var Modal = (function () {
 
   var trigger = $qsa('.modal__trigger'); // what you click to activate the modal
   var modals = $qsa('.modal'); // the entire modal (takes up entire window)
   var modalsbg = $qsa('.modal__bg'); // the entire modal (takes up entire window)
   var content = $qsa('.modal__content'); // the inner content of the modal
-	var closers = $qsa('.modal__close'); // an element used to close the modal
+  var closers = $qsa('.modal__close'); // an element used to close the modal
   var w = window;
   var isOpen = false;
-	var contentDelay = 400; // duration after you click the button and wait for the content to show
+  var contentDelay = 400; // duration after you click the button and wait for the content to show
   var len = trigger.length;
 
   // make it easier for yourself by not having to type as much to select an element
@@ -181,7 +187,7 @@ var Modal = (function() {
     return document.querySelectorAll(el);
   }
 
-  var getId = function(event) {
+  var getId = function (event) {
 
     event.preventDefault();
     var self = this;
@@ -196,7 +202,7 @@ var Modal = (function() {
     makeDiv(self, modal);
   };
 
-  var makeDiv = function(self, modal) {
+  var makeDiv = function (self, modal) {
 
     var fakediv = document.getElementById('modal__temp');
 
@@ -213,7 +219,7 @@ var Modal = (function() {
     }
   };
 
-  var moveTrig = function(trig, modal, div) {
+  var moveTrig = function (trig, modal, div) {
     var trigProps = trig.getBoundingClientRect();
     var m = modal;
     var mProps = m.querySelector('.modal__content').getBoundingClientRect();
@@ -236,29 +242,29 @@ var Modal = (function() {
     transX = Math.round(xc - trigProps.left - trigProps.width / 2);
     transY = Math.round(yc - trigProps.top - trigProps.height / 2);
 
-		// if the modal is aligned to the top then move the button to the center-y of the modal instead of the window
+    // if the modal is aligned to the top then move the button to the center-y of the modal instead of the window
     if (m.classList.contains('modal--align-top')) {
       transY = Math.round(mProps.height / 2 + mProps.top - trigProps.top - trigProps.height / 2);
     }
 
 
-		// translate button to center of screen
-		trig.style.transform = 'translate(' + transX + 'px, ' + transY + 'px)';
-		trig.style.webkitTransform = 'translate(' + transX + 'px, ' + transY + 'px)';
-		// expand temporary div to the same size as the modal
-		div.style.transform = 'scale(' + scaleX + ',' + scaleY + ')';
-		div.style.webkitTransform = 'scale(' + scaleX + ',' + scaleY + ')';
+    // translate button to center of screen
+    trig.style.transform = 'translate(' + transX + 'px, ' + transY + 'px)';
+    trig.style.webkitTransform = 'translate(' + transX + 'px, ' + transY + 'px)';
+    // expand temporary div to the same size as the modal
+    div.style.transform = 'scale(' + scaleX + ',' + scaleY + ')';
+    div.style.webkitTransform = 'scale(' + scaleX + ',' + scaleY + ')';
 
 
-		window.setTimeout(function() {
-			window.requestAnimationFrame(function() {
-				open(m, div);
-			});
-		}, contentDelay);
+    window.setTimeout(function () {
+      window.requestAnimationFrame(function () {
+        open(m, div);
+      });
+    }, contentDelay);
 
   };
 
-  var open = function(m, div) {
+  var open = function (m, div) {
 
     if (!isOpen) {
       // select the content inside the modal
@@ -282,14 +288,14 @@ var Modal = (function() {
     function hideDiv() {
       // fadeout div so that it can't be seen when the window is resized
       div.style.opacity = '0';
-      $(".modal__trigger--active").css("opacity","0");
+      $(".modal__trigger--active").css("opacity", "0");
       content.removeEventListener('transitionend', hideDiv, false);
     }
   };
 
-  var close = function(event) {
+  var close = function (event) {
 
-		event.preventDefault();
+    event.preventDefault();
     event.stopImmediatePropagation();
 
     var target = event.target;
@@ -304,32 +310,32 @@ var Modal = (function() {
 
       // make the hidden div visible again and remove the transforms so it scales back to its original size
       div.style.opacity = '1';
-      $(".modal__trigger--active").css("opacity","1");
+      $(".modal__trigger--active").css("opacity", "1");
       div.removeAttribute('style');
 
-			/**
-			* iterate through the modals and modal contents and triggers to remove their active classes.
-      * remove the inline css from the trigger to move it back into its original position.
-			*/
+      /**
+       * iterate through the modals and modal contents and triggers to remove their active classes.
+       * remove the inline css from the trigger to move it back into its original position.
+       */
 
-			for (var i = 0; i < len; i++) {
-				modals[i].classList.remove('modal--active');
-				content[i].classList.remove('modal__content--active');
-				trigger[i].style.transform = 'none';
+      for (var i = 0; i < len; i++) {
+        modals[i].classList.remove('modal--active');
+        content[i].classList.remove('modal__content--active');
+        trigger[i].style.transform = 'none';
         trigger[i].style.webkitTransform = 'none';
-				trigger[i].classList.remove('modal__trigger--active');
-			}
+        trigger[i].classList.remove('modal__trigger--active');
+      }
 
       // when the temporary div is opacity:1 again, we want to remove it from the dom
-			div.addEventListener('transitionend', removeDiv, false);
+      div.addEventListener('transitionend', removeDiv, false);
 
       isOpen = false;
 
     }
 
     function removeDiv() {
-      setTimeout(function() {
-        window.requestAnimationFrame(function() {
+      setTimeout(function () {
+        window.requestAnimationFrame(function () {
           // remove the temp div from the dom with a slight delay so the animation looks good
           div.remove();
         });
@@ -338,7 +344,7 @@ var Modal = (function() {
 
   };
 
-  var bindActions = function() {
+  var bindActions = function () {
     for (var i = 0; i < len; i++) {
       trigger[i].addEventListener('click', getId, false);
       closers[i].addEventListener('click', close, false);
@@ -346,7 +352,7 @@ var Modal = (function() {
     }
   };
 
-  var init = function() {
+  var init = function () {
     bindActions();
   };
 
@@ -357,3 +363,65 @@ var Modal = (function() {
 }());
 
 Modal.init();
+
+/* Smooth Scroll */
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        // console.log($(this).prop("href").split("#")[1]);
+
+        setTimeout(() => {
+
+
+          if ($(this).prop("href").split("#")[1] == "home" || $(this).prop("href").split("#")[1] == "about") {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000, function () {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              };
+            });
+          } else {
+            $('html, body').animate({
+              scrollTop: target.offset().top + 100
+            }, 1000, function () {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              };
+            });
+          }
+
+        }, 400);
+
+      }
+    }
+  });
